9d5290cd124bd999b28da22e5a928c9b596498a4
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2026-03-28 21:01:01 -0500

    fix: remove all usages of vp

--- src/vec.h
+++ src/vec.h
@@ -9,28 +9,28 @@
     free(d - 2);\
 } while (0)
 
-#define MK_VEC(vp) do {\
-    void* p = malloc(2 * sizeof(size_t) + sizeof(**vp));\
+#define MK_VEC(v) do {\
+    void* p = malloc(2 * sizeof(size_t) + sizeof(*v));\
     size_t* d = p;\
     *(d++) = 1;\
     *(d++) = 0;\
-    *vp = (void*)d;\
+    v = (void*)d;\
 } while (0)
 
-#define PUSH_VEC(vp, e) do {\
-    size_t* d = (void*)*vp;\
+#define PUSH_VEC(v, e) do {\
+    size_t* d = (void*)v;\
     size_t len = *(--d);\
     size_t cap = *(--d);\
     if (len >= cap) {\
         size_t newcap = 2 * cap;\
-        size_t* p = realloc(d, 2 * sizeof(size_t) + newcap * sizeof(**vp));\
+        size_t* p = realloc(d, 2 * sizeof(size_t) + newcap * sizeof(*v));\
         if (!p) break;\
         d = p;\
         *(d++) = newcap;\
     }\
     ++(*(d++));\
-    *vp = (void*) d;\
-    *vp[len] = e;\
+    v = (void*) d;\
+    v[len] = e;\
 } while (0)
 
 #endif
