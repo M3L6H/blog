dcb96ca2e2af158959702979a6a3f80fa65eea48
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2026-03-28 20:31:57 -0500

    feat: add PUSH_VEC and FREE_VEC

--- src/main.c
+++ src/main.c
@@ -19,18 +19,24 @@ int main(int argc, char* argv[]) {
     size_t len = 0;
     
     lex(&doc, &len);
-    
-    printf("%s\n", doc);
-    printf("len: %zu\n", len);
-    
     free(doc);
     
     char* myvec = NULL;
     MK_VEC(myvec);
+    PUSH_VEC(&myvec, 'h');
+    PUSH_VEC(&myvec, 'e');
+    PUSH_VEC(&myvec, 'l');
+    PUSH_VEC(&myvec, 'l');
+    PUSH_VEC(&myvec, 'o');
+    PUSH_VEC(&myvec, '\0');
     
     size_t* d = (void*)myvec;
-    printf("len: %zu", *(--d));
-    printf("cap: %zu", *(--d));
+    printf("len: %zu ", *(--d));
+    printf("cap: %zu\n", *(--d));
+    printf("%s", myvec);
+    printf("strlen: %zu\n", strlen(myvec));
+    
+    FREE_VEC(myvec);
  
     return EXIT_OK;
 }
