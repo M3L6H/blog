6fd8ec3836b27a58a3c8c1903a084238c330cd29
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2026-04-07 22:59:26 -0500

    chore: define initial tokens

--- src/lexer.h
+++ src/lexer.h
@@ -3,6 +3,16 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+#define TOK_NL 0
+#define TOK_SP 1
+#define TOK_WD 2
+#define TOK_H1 10
+#define TOK_H2 11
+#define TOK_H3 12
+#define TOK_H4 13
+#define TOK_H5 14
+#define TOK_H6 15
+
 typedef struct {
   unsigned int t;
   size_t a;
