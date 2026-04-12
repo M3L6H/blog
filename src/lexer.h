425552ec247c5742e152ad988fbb53e846165c55
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2026-04-11 22:55:39 -0500

    chore: add inline tokens

--- src/lexer.h
+++ src/lexer.h
@@ -12,6 +12,13 @@
 #define TOK_H4 13
 #define TOK_H5 14
 #define TOK_H6 15
+#define TOK_ITL 20
+#define TOK_BLD 21
+#define TOK_STK 22
+#define TOK_COD 23
+#define TOK_SUP 24
+#define TOK_SUB 25
+#define TOK_LTX 26
 #define TOK_LCB 40
 #define TOK_RCB 41
 #define TOK_LP 42
